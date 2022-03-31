9ecb9363c915b72bad55a5a211828ebdddc49d1c
GeorgeDong32 <98630204+GeorgeDong32@users.noreply.github.com>
2022-03-31 14:44:33 +0800

    Update week6_课后2_my_macro.hpp

--- week6_课后2_my_macro.hpp
+++ week6_课后2_my_macro.hpp
@@ -1,3 +1,4 @@
 //Copyright (c) GeorgeDong32(Github). All rights reserved.
 #define MACRO_MAX(a,b) (a > b ? a : b);
 #define MACRO_SWAP(a,b) a ^= b ^= a ^= b;
+//Copyright (c) GeorgeDong32(Github). All rights reserved.
