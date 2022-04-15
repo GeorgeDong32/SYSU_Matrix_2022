6a68ebf33d3f4baaea06c475df87653673554156
GeorgeDong32 <georgedong32@foxmail.com>
2022-04-15 10:58:38 +0800

    add week9 class

---
+++ week9_课堂1_BASE.cpp
@@ -0,0 +1,19 @@
+#include "BASE.h"
+
+BASE::BASE(int p1, int p2)
+{
+	mem1 = p1;
+	mem2 = p2;
+}
+
+int BASE::inc1()
+{
+	mem1++;
+	return 1;
+}
+
+void BASE::display() const
+{
+	cout << "mem1=" << mem1 << ",";
+	cout << "mem2=" << mem2 << endl;
+}
