3cc3615c5bd4b266e0366860a1072a86c9a65d52
GeorgeDong32 <98630204+GeorgeDong32@users.noreply.github.com>
2022-04-15 11:00:20 +0800

    Update week9_课堂1_DERIVED.cpp

--- week9_课堂1_DERIVED.cpp
+++ week9_课堂1_DERIVED.cpp
@@ -1,5 +1,9 @@
 #include "DERIVED.h"
-
+/*********************************************************
+*                                                        *
+* Copyright(c) GeorgeDong32(Github).All rights reserved. *
+*                                                        *
+**********************************************************/
 DERIVED::DERIVED(int x1, int x2, int x3, int x4, int x5): BASE(x1, x2), mem4(x3, x4), mem3(x5) {}
 
 int DERIVED::inc1()
