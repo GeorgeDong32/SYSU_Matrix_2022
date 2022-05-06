b379f2a642d72be4c94ba322679032247097cecd
GeorgeDong32 <georgedong32@foxmail.com>
2022-05-06 13:08:31 +0800

    add week12

---
+++ week12_课后3_ab.h
@@ -0,0 +1,33 @@
+#pragma once
+#include <iostream>
+using std::cout; using std::endl;
+/*********************************************************
+*                                                        *
+* Copyright(c) GeorgeDong32(Github).All rights reserved. *
+*                                                        *
+**********************************************************/
+class A {
+public:
+	virtual void Prin() {
+		cout << "Prin come form class A" << endl;
+	}
+	virtual ~A() { cout << "A::~A()called" << endl; }
+};
+
+class B: public A{
+	char* buf;
+public:
+	void Prin() {
+		cout << "Prin come from class B" << endl;
+	}
+	B() : A() { buf = nullptr; }
+	B(int i) : A() {
+		buf = (char*) new int;
+		*buf = i;
+	}
+	~B() {
+		cout << "B::~B()called" << endl;
+		if (buf != nullptr)
+			delete buf;
+	}
+};
