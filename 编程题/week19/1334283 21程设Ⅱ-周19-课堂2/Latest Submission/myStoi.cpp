71eda596cc83ca3740fd96fc83cac5b07c1e26bf
FrozenZongzi <107297940+FrozenZongzi@users.noreply.github.com>
2022-06-24 11:29:16 +0800

    Update myStoi.cpp

--- 编程题/week19/1334283 21程设Ⅱ-周19-课堂2/Latest Submission/myStoi.cpp
+++ 编程题/week19/1334283 21程设Ⅱ-周19-课堂2/Latest Submission/myStoi.cpp
@@ -8,6 +8,7 @@
 int myStoi(string dnum)
 {
 	int re = 0;
+	//检查是否有非法字符
 	int sl = dnum.length(); int checks = 1;
 	for (int i = 0; i < sl; i++)
 	{
@@ -27,22 +28,24 @@ int myStoi(string dnum)
 			checks *= 1;
 		}
 	}
+	//调用库中stoi函数
 	try
 	{
 		re = stoi(dnum);
 	}
-	catch (std::invalid_argument&)
+	catch (std::invalid_argument&)//系统判定非法输入
 	{
 		throw MyException("Exception : invalid stoi argument").what();
 	}
-	catch (std::out_of_range&)
+	catch (std::out_of_range&)//系统判定越界
 	{
 		throw MyException("Exception : stoi argument out of range").what();
 	}
-	catch (...)
+	catch (...)//防止其他出错
 	{
 		return re;
 	}
+	//如有特殊字符且stio未报错，主动报错（依题意）
 	if (checks == 0)
 	{
 		throw MyException("Exception : invalid stoi argument").what();
